028212ffa2ff81f4e593afa2bd6d2bf1026f4c0d
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-11-13 20:42:26 +0000

    Se termina ejercicio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Plantilla·D
+# Plantilla 3D
 Plantilla base para proyectos en 3D con P5.JS
--- sketch.js
+++ sketch.js
@@ -1,17 +1,27 @@
+let fondo
+let textura
+
 function preload() {
   // put preload code here
+  fondo = loadImage("./images/alunizaje.png")
+  textura = loadImage("./images/tierra.jpg")
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800, WEBGL)
+  createCanvas(1200,720, WEBGL)
+  imageMode(CENTER)
 }
 
 function draw() {
   // put drawing code here
   background(255)
-  rotateX(frameCount * 0.01)
-  rotateY(frameCount * 0.01)
-  rotateZ(frameCount * 0.01)
-  box(150)
+  image(fondo, 0, 0, width, height)
+  translate(300,-300, 0 )
+  rotateX(frameCount * 0.001)
+  rotateY(frameCount * 0.001)
+  rotateZ(frameCount * 0.001)
+  texture(textura)
+  sphere(80,24)
+  //box(80,40)
 }
